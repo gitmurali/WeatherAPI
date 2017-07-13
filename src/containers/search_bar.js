4d3c951880abef1827c40c7102e0678e351d7c28
Murali Nutalapati <info@muraliprashanth.me>
2017-07-13 13:40:44 +0530

    adds the reduxPromise middleware and implements axios promise

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = { term:'' };
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({
+			term: event.target.value
+		})
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
+	}
+
     render() {
         return (
-            <form className="input-group">
-                <input className="form-control" />
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input className="form-control"
+					   value={this.state.term}
+					   placeholder="get 5 day forecast in your favourite cities"
+					   onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Search</button>
                 </span>
@@ -14,7 +40,11 @@ class SearchBar extends Component {
     }
 }
 
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather: fetchWeather }, dispatch); // dispatch make sures that action creator passes thru the middleware and then reducers.
+}
+
 SearchBar.propTypes = {};
 SearchBar.defaultProps = {};
 
-export default SearchBar;
+export default connect(null, mapDispatchToProps)(SearchBar);
