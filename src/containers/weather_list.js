590d5f9fcdc548183ed6b814afddd991cffde9fb
Murali Nutalapati <info@muraliprashanth.me>
2017-07-13 19:07:37 +0530

    Integrates google maps instead of just city name

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_maps';
 
 
 class WeatherList extends Component {
@@ -11,10 +12,11 @@ class WeatherList extends Component {
 		const temps = cityData.list.map(weather => weather.main.temp);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humiditys = cityData.list.map(weather => weather.main.humidity);
+		const { lon, lat } = cityData.city.coord;
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td><GoogleMap lon={lon} lat={lat}/></td>
 				<td>
 					<Chart data={temps} color="red" units="k"/>
 				</td>
